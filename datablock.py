1f258c77dce864b559c9c18d78da53a9f075ec02
Peter Chervenski <spookey@abv.bg>
2023-03-01 21:29:29 +0200

    overnight_move fix

--- datablock.py
+++ datablock.py
@@ -201,7 +201,7 @@ def procdata(ddd,
         # Calculate the "overnight move" indicator
         overnight_move = []
         last_open = None
-        for i, (xopen_, date) in enumerate(zip(data['open'].values, dates)):
+        for i, (xopen_, date) in enumerate(zip(open_.values, dates)):
             if i == 0 or date != dates[i-1] and last_open is not None:
                 overnight = xopen_ - last_open
                 overnight_move.append(overnight)
